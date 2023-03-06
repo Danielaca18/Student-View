36f61436f94d171fafc899fb4f54d505a19c995f
Daniel Castro <dcastro@ualberta.ca>
2023-03-06 00:37:25 -0700

    Initial Commit

---
+++ Student View/Student_ViewApp.swift
@@ -0,0 +1,20 @@
+//
+//  Student_ViewApp.swift
+//  Student View
+//
+//  Created by Daniel Castro on 2023-03-06.
+//
+
+import SwiftUI
+
+@main
+struct Student_ViewApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
