07904a513aabf4b2b91d669431bcc713ce13c384
Daniel Castro <dcastro@ualberta.ca>
2023-03-22 00:10:04 -0600

    Added icons

--- Student ViewUITests/Student_ViewUITests.swift
+++ Student ViewUITests/Student_ViewUITests.swift
@@ -29,6 +29,11 @@ final class Student_ViewUITests: XCTestCase {
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testAssignmentAdd() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
